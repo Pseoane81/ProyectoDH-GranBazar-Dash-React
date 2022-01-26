ba718b2043775dd79a087b80109687ba765142e8
Pablo Seoane <pablitoseoane@gmail.com>
2022-01-26 18:56:55 -0300

    funcionalidad de last product

--- src/components/Cards/LastProductInDb.js
+++ src/components/Cards/LastProductInDb.js
@@ -2,18 +2,16 @@ import React, { useState, useEffect } from "react";
 
 
 function LastProductInDb() {
-    const [products, setproducts] = useState([]);
+    const [lastProduct, setpLastPoducts] = useState([]);
 
     useEffect(() => {   
 		fetch("http://localhost:3001/api/products")
 		.then(response => response.json())
 		.then(data => {
-			setproducts(data.products)
+			setpLastPoducts(data.products[data.products.length-1])
 		})
 	},[])
-
-    let cantidad = products.length
-    console.log(products[cantidad - 1])
+    
 
 
 
@@ -25,11 +23,11 @@ function LastProductInDb() {
                 <h5 className="m-0 font-weight-bold text-gray-800">Ultimo producto en la base de datos</h5>
             </div>
             <div className="card-body">
-                <div className="text-center">
-                    <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} /*src={products[cantidad - 1].img}*/ alt="algo" />
+                <div className="text-center" >
+                    <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={lastProduct.img}  alt="algo" />
                 </div>
-                <p></p>
-                <a className="btn btn-danger" target="_blank" /*href={products[cantidad - 1].detail}*/ rel="nofollow" >Ver detalle de producto</a>
+                <p>{lastProduct.descripcion} </p>
+                <a className="btn btn-danger" target="_blank" href={lastProduct.detail}  rel="nofollow">Ver detalle de producto</a>
             </div>
         </div>
         </div>
