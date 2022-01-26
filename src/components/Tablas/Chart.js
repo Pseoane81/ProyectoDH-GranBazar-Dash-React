91df07695814c0b8252ed70ebb04c29d9a8eb560
Pablo Seoane <pablitoseoane@gmail.com>
2022-01-26 20:04:08 -0300

    cuadro con datos

--- src/components/Tablas/Chart.js
+++ src/components/Tablas/Chart.js
@@ -1,26 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import ChartRow from './ChartRow';
 
-let tableRowsData = [
-    {
-        Title: 'Billy Elliot ',
-        Length: '123',
-        Rating: '5',
-        Categories: ['Drama','Comedia'],
-        Awards: 2
-    },
-    {
-        Title: 'Alicia en el país de las maravillas',
-        Length: '142',
-        Rating: '4.8',
-        Categories: ['Drama','Acción','Comedia'],
-        Awards: 3
-    },
-    
-]
+function Chart() {
+    const [tableRowsData, setptableRowsData] = useState([]);
 
+    useEffect(() => {   
+		fetch("http://localhost:3001/api/products")
+		.then(response => response.json())
+		.then(data => {
+			setptableRowsData(data.products)
+		})
+	},[])
 
-function Chart (){
     return (
         /* <!-- DataTales Example --> */
         <div className="card shadow mb-4">
@@ -29,20 +20,20 @@ function Chart (){
                     <table className="table table-bordered" id="dataTable" width="100%" cellSpacing="0">
                         <thead>
                             <tr>
-                                <th>Título</th>
-                                <th>Duración</th>
-                                <th>Rating</th>
-                                <th>Género</th>
-                                <th>Premios</th>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Categoria</th>
+                                <th>Origen</th>
+                                <th>Color</th>
                             </tr>
                         </thead>
                         <tfoot>
                             <tr>
-                                <th>Título</th>
-                                <th>Duración</th>
-                                <th>Rating</th>
-                                <th>Género</th>
-                                <th>Premios</th>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Categoria</th>
+                                <th>Origen</th>
+                                <th>Color</th>
                             </tr>
                         </tfoot>
                         <tbody>
--- src/components/Tablas/ChartRow.js
+++ src/components/Tablas/ChartRow.js
@@ -4,17 +4,27 @@ import React from 'react';
 function ChartRow(props){
     return (
                 <tr>
-                    <td>{props.Title}</td>
-                    <td>{props.Length}</td>
-                    <td>{props.Rating}</td>
+                    <td>{props.id}</td>
+                    <td>{props.name}</td>
                     <td>
                         <ul>
-                            {props.Categories.map( (category,i) => 
-                                <li key={`category ${i}`}>{category}</li>
+                            {props.categoria.map( (category,i) => 
+                                <li key={`category ${i}`}>{category.category}</li>
                             )}
                         </ul>
                     </td>
-                    <td>{props.Awards}</td>
+                    <td>{props.name}</td>
+                    <td>
+                        <ul>
+                            {props.color.map( (category,i) => 
+                                <li key={`category ${i}`}>{category.color}</li>
+                            )}
+                        </ul>
+                    </td>
+
+                    
+                    
+                    
                 </tr>
             )
     }
